ccadc83525d076f86086f815416f88c3105e9322
Elison Liu <elisonliu97@gmail.com>
2021-05-28 01:41:53 -0400

    created function to create time block

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// VARIABLES
+var today = moment()
+var timeBlockCont = $('.container');
+
+
+// function to display the date at the top
 function displayDay(){
-    var today = moment();
     $("#currentDay").text(today.format("MMM Do, YYYY"));
+}
+
+// function to create the timeblocks
+function createTimeBlock(){
+    var timeBlock = $('<div>');
+    timeBlockCont.append(timeBlock)
+
+    var timeBlockName = $('<p>');
+    timeBlockName.text("Hello")
+    timeBlock.append(timeBlockName)
+    
+    var timeBlockSubmit = $('<input>')
+    timeBlockName.after(timeBlockSubmit)
+    timeBlock.append(timeBlockName)
+    
+    var timeBlockButton = $('<button>')
+    timeBlockButton.text("LOCK")
+    timeBlock.append(timeBlockButton)
 }
 
 displayDay();
